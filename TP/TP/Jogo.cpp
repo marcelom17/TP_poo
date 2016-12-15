2342ed60c5434854ce5ba301120d72539e617a81
Marcelo <marcelomacedo17@gmail.com>
2016-12-15 17:53:18 +0000

    vou fazer agora os comandos

--- TP/TP/Jogo.cpp
+++ TP/TP/Jogo.cpp
@@ -1,5 +1,6 @@
 #include "Jogo.h"
 
+
 void Jogo::Inicia_jogo(){
 	//limpar o ecra
 
@@ -13,4 +14,137 @@ int Jogo::avanca_instancia(int n){
 	return instancia;
 	//ou em ciclo
 
+}
+
+void Jogo::desenha_tabuleiro(){
+	string comando;
+	bool jogar = true;
+	
+	while (jogar) {
+		c.gotoxy(0,44);
+		c.setBackgroundColor(c.PRETO);
+		c.gotoxy(0, 45);
+		c.setBackgroundColor(c.BRANCO);
+		c.setTextColor(c.PRETO);
+		cout << "Comando: ";
+		for (unsigned int i = 1; i < 110; i++)
+			cout << " ";
+		//Sleep(1500);
+		limpa_consola(c, 0, 0);
+		c.setTextColor(c.BRANCO_CLARO);
+		c.gotoxy(16, 45);
+		getline(cin, comando);
+
+
+		if (comando == "")
+			comando = "NEXT";
+
+		jogar = testa_jogada(comando);
+	}
+	
+
+
+}
+
+void Jogo::limpa_consola(Consola & consola, int cor, bool flag){
+	if (!flag)	// Pinta a área da prompt com a cor de fundo da consola
+	{
+		consola.gotoxy(8, 50);
+		cout << "                                                                           ";
+		consola.gotoxy(0, 51);
+		cout << "                                                                                ";
+		consola.gotoxy(0, 52);
+		cout << "                                                                                ";
+
+	}
+	else			// Pinta a área da prompt com a cor da fação que está a jogar
+	{
+		consola.setBackgroundColor(cor);
+		consola.gotoxy(8, 36);
+	}
+}
+
+bool Jogo::comandos_configurar(string comando)
+{
+	string buffer;
+	stringstream cmd(comando);	// insere o comando numa stream
+	vector <string> comandos;	// cria um vetor onde serao armazenados as palavras isuladas
+
+	while (cmd >> buffer)		// Enquanto houver comandos para ler vai armazenando no vetor ( FAZ O PARSE DO COMANDO)
+		comandos.push_back(buffer);
+
+	string cmd_aux = upper(comandos[0]);
+
+	if (cmd_aux == "SAIR") {
+		exit(0);
+
+	}
+//por no comandos_jogo
+/*	if (cmd_aux == "NEXT") {
+		if (comandos.size() == 2)
+			proximaInstancia(atoi(comandos[1].c_str()));
+		else
+			proximaInstancia(1);
+		return true;
+	}
+	*/
+	if (cmd_aux == "DIM") {
+		dimensao(comandos);
+	}
+
+	if (cmd_aux == "MOEDAS") {
+
+	}
+
+	if (cmd_aux == "OPONETES") {
+
+	}
+
+	if (cmd_aux == "CASTELO") {
+
+	}
+
+	if (cmd_aux == "MKPERFIL") {
+
+	}
+
+	if (cmd_aux == "ADDPERFIL") {
+
+	}
+
+	if (cmd_aux == "SUBPERFIL") {
+
+	}
+
+	if (cmd_aux == "RMPERFIL") {
+
+	}
+
+	if (cmd_aux == "LOAD") {
+
+	}
+
+	if (cmd_aux == "INICIO") {
+
+	}
+
+	return false;
+}
+
+string Jogo::upper(string palavra)
+{
+	for (unsigned int i = 0; i < palavra.size(); i++)
+	{
+		if (palavra.at(i) >= 'a' && palavra.at(i) <= 'z')
+			palavra.at(i) -= 32;
+	}
+	return palavra;
+}
+
+void Jogo::proximaInstancia(int n){
+	for (int i = 0; i < n; i++) {
+		//falta os movimentos dos seres
+
+		instancia++;
+	}
 }
--- TP/TP/Jogo.h
+++ TP/TP/Jogo.h
@@ -1,8 +1,31 @@
 #pragma once
 
+#include "Util.h"
+#include "Consola.h"
+
 class Jogo {
+	Consola c;
 	int instancia=0;
 public:
 	void Inicia_jogo();
 	int avanca_instancia(int n);
+	void desenha_tabuleiro();
+	bool comandos_configurar(string comando);
+	bool comandos_jogo(string comando);
+	void limpa_consola(Consola & consola, int cor, bool flag);
+	string upper(string palavra);
+	void proximaInstancia(int n);
+
+
+// comandos de configuracao
+	void dimensao(vector<string>comandos);
+	void moedas(vector<string>comandos);
+	void oponentes(vector<string>comandos);
+	void castelo(vector<string>comandos);
+	void mkperfil(vector<string>comandos);
+	void addperfil(vector<string>comandos);
+	void subperfil(vector<string>comandos);
+	void rmperfil(vector<string>comandos);
+	void load(vector<string>comandos);
+	void inicio();
 };
